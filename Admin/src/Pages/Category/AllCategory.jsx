a2e64640691abad163df356c688969a2634e9d08
mannumishra <mannumishra2207@outlook.com>
2024-09-29 17:18:31 +0530

    add chnages

---
+++ Admin/src/Pages/Category/AllCategory.jsx
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom'
+import Swal from 'sweetalert2';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const AllCategory = () => {
+    const [data, setData] = useState([])
+
+    const getApiData = async () => {
+        try {
+            const res = await axios.get("https://www.api.increasecard.shop/v1/get-personal-details")
+            console.log(res)
+            if (res.status === 200) {
+                const newData = res.data.data
+                setData(newData.reverse())
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getApiData()
+    }, [data.length])
+
+    const  deletePersonalDetails = async(id)=>{
+        try {
+            const res = await axios.delete("https://www.api.increasecard.shop/v1/delete-personal-details/"+id)
+            console.log(res)
+            if (res.status === 200) {
+                toast.success("Personal Details Delete Successfully")
+                getApiData()
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    return (
+        <>
+            <ToastContainer />
+            <div className="bread">
+                <div className="head">
+                    <h4>Personal List </h4>
+                </div>
+            </div>
+
+            <div className="filteration">
+                <div className="selects">
+                    {/* <select>
+                        <option>Ascending Order </option>
+                        <option>Descending Order </option>
+                    </select> */}
+                </div>
+                <div className="search">
+                    <label htmlFor="search">Search </label> &nbsp;
+                    <input type="text" name="search" id="search" />
+                </div>
+            </div>
+
+            <section className="d-table ">
+                <table class="table table-bordered table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">Sr.No.</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Phone</th>
+                            <th scope="col">Pan Card</th>
+                            <th scope="col">Postal Code</th>
+                            <th scope="col">Refrence Number</th>
+                            <th scope="col">Time & Date</th>
+                            <th scope="col">Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            data.map((item,index) =>
+                                <tr key={index}>
+                                    <th scope="row">{index+1}</th>
+                                    <td>{item.name}</td>
+                                    <td>{item.email}</td>
+                                    <td>{item.number}</td>
+                                    <td>{item.panCardNumber}</td>
+                                    <td>{item.postalCode}</td>
+                                    <td>{item.referenceNumber}</td>
+                                    <td>{new Date(item.createdAt).toLocaleString()}</td>
+                                    <td><Link className="bt delete" onClick={()=>deletePersonalDetails(item._id)}>Delete <i class="fa-solid fa-trash"></i></Link></td>
+                                </tr>
+                            )
+                        }
+                    </tbody>
+
+                </table>
+            </section>
+        </>
+    )
+}
+
+export default AllCategory
---
+++ Admin/src/Pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <>
+      <div>
+        <h2 className='text-center mt-3'>Welcome To RBL BANK Admin Panel</h2>
+      </div>
+    </>
+  )
+}
+
+export default Dashboard
